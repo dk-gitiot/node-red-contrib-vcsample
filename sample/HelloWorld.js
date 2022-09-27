a8cfafacc3cecb03e7c2b26d192ef3655decfe74
dk-gitiot <113344313+dk-gitiot@users.noreply.github.com>
2022-09-27 21:35:04 +0530

    Add files via upload

---
+++ sample/HelloWorld.js
@@ -0,0 +1,13 @@
+module.exports = function(RED) {
+    function helloWorld(config) {
+        RED.nodes.createNode(this,config);
+        var context = this.context();
+        var node = this;
+        this.on('input', function(msg){
+        
+            var outMsg = {payload: "Hello World"+JSON.stringify(msg)};
+            node.send(outMsg);
+        });
+    }
+    RED.nodes.registerType("Hello World",helloWorld);
+};
